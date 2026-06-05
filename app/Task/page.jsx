b7a2554774d25e3d539d644aedd84d414bee5359
AmusaAbdul <amusaabdullah1234@gmail.com>
2026-06-05 15:26:20 +0100

    Second commit

--- app/Task/page.jsx
+++ app/Task/page.jsx
@@ -32,7 +32,6 @@ export default function page() {
         const newNotes = {text: note, id: crypto.randomUUID()}
         setNotes(prev => [...prev, newNotes]);
         setNote("") 
-        console.log(note)
     }
 
     const toggleComplete = (id) => {
@@ -79,7 +78,8 @@ export default function page() {
         setEdit(null)
         setNote("")
     }
-
+    const getWordCount = note.length
+    const getTodoCount = task.length
 
     const today = new Date().toISOString().split("T")[0];
 
@@ -113,7 +113,7 @@ export default function page() {
                 <button onClick={() => setDarkMode(!darkMode)}> {darkMode ? "☀️" : "🌙"}</button>
             </div>
             <div className="mt-5 flex flex-col gap-5">
-                <input className="border-2 border-white dark:border-blue-950 rounded-full p-2 w-full outline-none" type="search" placeholder="Search" />
+                <input className="border-2 border-white dark:border-blue-950 text-xs rounded-full p-2 w-full outline-none" type="search" placeholder="Search" />
                 <div className="flex gap-5 flex-wrap ">
                     <button onClick={() => setFilter("All")} value="All" className={`${filter === "All" ? "bg-blue-950 dark:bg-amber-50  text-white dark:text-blue-950" : "bg-amber-50 dark:bg-blue-950"} p-3 rounded-full text-[0.8rem]`}>All</button>
                     <button onClick={() => (setFilter("Work"), setCategory("Work"))} value="Work" className={`${filter === "Work" ? "bg-blue-950 dark:bg-amber-50  text-white dark:text-blue-950" : "bg-amber-50 dark:bg-blue-950"} p-3 rounded-full text-[0.8rem]`}>Work</button>
@@ -132,11 +132,14 @@ export default function page() {
                         }`}>
                         Add
                     </button>
-                    {(!task.trim() || !date) && (
-                        <p className="text-xs text-red-400">
-                            Please enter todo and date
-                        </p>
-                    )}
+                    <div className="flex justify-between">
+                        {(!task.trim() || !date) && (
+                            <p className="text-xs text-red-400">
+                                Please enter todo and date
+                            </p>
+                        )}
+                        <p className="text-xs ">({getTodoCount} {getTodoCount === 0 ? "word" : "words"})</p>
+                    </div>
                     <input
                         type="date"
                         value={date}
@@ -148,20 +151,20 @@ export default function page() {
                 <ol className="list-decimal mt-5 flex flex-col gap-2 overflow-y-auto max-h-80">
                     {filteredTask.map((t, index) => (
                         <div key={index} className="">
-                            <div className={`flex  flex-col gap-5 text-[0.8rem]  bg-white dark:bg-blue-950 shadow-sm border border-gray-200 dark:border-blue-900 rounded-2xl p-4 hover:shadow-md transition justify-between`}>
+                            <div className={`flex pl-7 flex-col gap-5 text-[0.8rem]  bg-white dark:bg-blue-950 shadow-sm border border-gray-200 dark:border-blue-900 rounded-2xl p-4 hover:shadow-md transition justify-between`}>
                                 {edit === t.id ?
                                     <div className="flex flex-col gap-2">
                                         <textarea className={`border-2 text-[0.8rem] h-40 w-full flex-1 outline-none border-blue-950 dark:border-blue-900 p-3`}
                                             type="text" value={task} onChange={(e) => setTask(e.target.value)} />
-                                        <button onClick={() => handleSave(t.id)} className="bg-green-500 text-white p-1 font-light">Save</button>
+                                        <button onClick={() => handleSave(t.id)} className="bg-green-500 text-white p-1 font-light hover:bg-green-700">Save</button>
                                     </div> :
                                     <li className={`${t.completed ? "line-through " : ""} `}> {t.text}</li>
                                 }
                                 <hr className="text-blue-900" />
                                 <div className="flex gap-2">
-                                    <button onClick={() => { setTask(t.text), setEdit(t.id) }} className="bg-green-500 text-white p-1 font-light">Edit</button>
+                                    <button onClick={() => { setTask(t.text), setEdit(t.id) }} className="bg-green-500 text-white p-1 font-light hover:bg-green-700">Edit</button>
                                     <input checked={t.completed ?? false} onChange={() => toggleComplete(t.id)} type="checkbox" />
-                                    <button onClick={() => deleteItem(t.id)} key={index} className="bg-red-500 text-amber-50 p-1 font-light">Del</button>
+                                    <button onClick={() => deleteItem(t.id)} key={index} className="bg-red-500 text-amber-50 p-1 font-light hover:bg-red-700">Del</button>
                                 </div>
 
                             </div>
@@ -215,8 +218,11 @@ export default function page() {
             </div>
             <div className={` mt-10 ${filter === "All" || filter === "Personal" || filter === "Work" || filter === "Wishlist" ? "hidden" : ""}`}>
                 <textarea value={note} onChange={(e) => setNote(e.target.value)} className={`border-2 text-[0.8rem] w-full h-40 flex-1 outline-none border-blue-950 dark:border-blue-900 p-3`} name="note" id="note"/>
-                {(!note) && (<p className="text-xs text-red-400 mt-2">Textarea empty</p>)}
-                <div className="flex justify-end w-full ">
+                <div className="flex justify-between mt-2">
+                    {(!note) && (<p className="text-xs text-red-400 ">Textarea's empty</p>)}
+                    <p className="text-xs ">({getWordCount} {getWordCount === 0 ? "word" : "words"})</p>
+                </div>
+                <div className="flex justify-end w-full mt-2">
                     <button type="button" onClick={handleNote} 
                         className={`${!note.trim() ?
                         "bg-gray-400 cursor-not-allowed" : 
@@ -232,12 +238,12 @@ export default function page() {
                                     <div className="flex flex-col gap-2">
                                         <textarea className={`border-2 text-[0.8rem] w-full flex-1 outline-none h-40 border-blue-950 dark:border-blue-900 p-3`}
                                         type="text" value={note} onChange={(e) => setNote(e.target.value)} />
-                                        <button type="button" onClick={() => handleSaveNote(n.id)} className="bg-green-500 text-white p-1 font-light">Save</button>
+                                        <button type="button" onClick={() => handleSaveNote(n.id)} className="bg-green-500 text-white p-1 font-light hover:bg-green-700">Save</button>
                                     </div> :
                                   <span>{n.text}</span>}
                                 <div className="flex justify-end  mt-5 gap-3">
-                                    <button onClick={() => (setNote(n.text), setEdit(n.id))} className="bg-green-500 text-amber-50 p-1 text-sm font-light">Edit</button>
-                                    <button onClick={() => deleteNote(n.id)} className="bg-red-500 text-sm text-amber-50 p-1 font-light">Del</button>
+                                    <button onClick={() => (setNote(n.text), setEdit(n.id))} className="bg-green-500 text-amber-50 p-1 text-sm font-light hover:bg-green-700">Edit</button>
+                                    <button onClick={() => deleteNote(n.id)} className="bg-red-500 text-sm text-amber-50 p-1 font-light hover:bg-red-700">Del</button>
                                 </div>
                             </li>
                         </div>
